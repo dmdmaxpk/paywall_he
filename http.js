7384aeeca9efb4e133e9c6a61c999090d0636870
Usama <musamadev@gmail.com>
2019-05-27 14:53:27 +0500

    initial commit

---
+++ http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+// Creating server
+const server = http.createServer((req, res) => {
+	console.log(`URL requested: ${req.url}`);
+
+	// Setting header
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.writeHead(200, { 'X-Custom': 'goonj' });
+
+	console.log('Number: ', req.headers['x-msisdn']);
+
+	// Set the response content
+	let msisdn = req.headers['x-msisdn'] ? req.headers['x-msisdn'] : null;
+	res.write(JSON.stringify({ msisdn }));
+
+	// End response
+	res.end();
+});
+
+// Listening on port
+const port = 5005;
+server.listen(port, () => console.log(`Node.js web server running at port ${port}`));
